edc6930b610dea7cd3d4cf5f27c27494cafbdb2f
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-05-27 14:54:09 -0700

    Add time tracker for training and prediction time

--- Proj1-NaiveBayes/nb_author_id.py
+++ Proj1-NaiveBayes/nb_author_id.py
@@ -31,9 +31,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 clf = GaussianNB()
 
+t0 = time()
 clf = clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t1 = time()
 y_pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
 
 #Accuracy
 accuracy = accuracy_score(labels_test, y_pred)

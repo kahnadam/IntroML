ade21e9899ff69162afadc648f0b10bebdd4d746
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-05-30 17:26:05 -0700

    Reduce training data size by 99%

--- Proj2-SVM/svm_author_id.py
+++ Proj2-SVM/svm_author_id.py
@@ -28,6 +28,8 @@ from sklearn.svm import SVC
 clf = SVC(kernel="linear", C=1.0, gamma='auto')
 
 t0 = time()
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0,3), "s"
 

c26cf8ea1df1c6a35513239faafd1e48bdc1ee6c
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-06-18 18:01:44 -0700

    Try stemmer on a few words

--- Proj10-TextLearning/stemmer.py
+++ Proj10-TextLearning/stemmer.py
@@ -1,2 +1,6 @@
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("english")
+
+print stemmer.stem("responsiveness")
+print stemmer.stem("responsivity")
+print stemmer.stem("unresponsive")

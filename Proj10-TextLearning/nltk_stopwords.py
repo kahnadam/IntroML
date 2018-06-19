9ba6ce2c4af2ef4d3fb3c57b1a8d3658705d05f6
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2018-06-18 17:56:43 -0700

    Remove workaround to install

--- Proj10-TextLearning/nltk_stopwords.py
+++ Proj10-TextLearning/nltk_stopwords.py
@@ -1,10 +1,2 @@
-#from nltk.corpus import stopwords
-#sw = stopwords.words("english")
-
-import os, ssl
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)): 
-    ssl._create_default_https_context = ssl._create_unverified_context
-
 import nltk
 nltk.download('all', halt_on_error=False)
